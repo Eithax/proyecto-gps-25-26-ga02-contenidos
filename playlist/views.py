67e4c1443cc7bbb6c47e621068064fe3f05328de
Santi <santiramb@gmail.com>
2025-11-25 10:45:33 +0100

    GA02-301 Feat: Create playlist endpoints #done #comment Se han implementado los endpoints CRUD para las listas de reproducción #time 2h Tiempo total empleado

--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from playlist.models import Playlist
+from playlist.permissions import IsAuthenticatedAndValidUser, IsPlaylistOwner
+from playlist.serializers import PlaylistSerializer
 
-# Create your views here.
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedAndValidUser, IsPlaylistOwner]
+    serializer_class = PlaylistSerializer
+
+    def get_queryset(self):
+        # Filtrar por el user_id (string) del usuario autenticado
+        return Playlist.objects.filter(owner_id=str(self.request.user.id))
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            from .serializers import PlaylistCreateSerializer
+            return PlaylistCreateSerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        # Asignar el owner_id desde el usuario autenticado (string)
+        serializer.save(owner_id=str(self.request.user.id))
+
+    @action(detail=False, methods=['get'])
+    def my_playlists(self, request):
+        """Listar playlists del usuario actual"""
+        playlists = self.get_queryset()
+        page = self.paginate_queryset(playlists)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(playlists, many=True)
+        return Response(serializer.data)
